6ba1de32d80bbb25b27991a48510456e489a073c
German <oswaldo101.gr@gmail.com>
2022-06-13 21:33:19 -0600

    Initial commit

---
+++ src/main/java/com/example/rp18045_parcial3_backend/Rp18045Parcial3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rp18045_parcial3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rp18045Parcial3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rp18045Parcial3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rp18045_parcial3_backend/Rp18045Parcial3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rp18045_parcial3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rp18045Parcial3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
